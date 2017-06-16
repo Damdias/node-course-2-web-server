8636b3e4d0d161c6da8194a1f5c817c4e7035026
damith <damithasanka@gmail.com>
2017-06-16 12:16:09 +0530

    initial code

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const hsb = require("hbs");
+const port = process.env.port || 3000;
+//app.set('view engine','html');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+    res.render("home.hbs", {
+        pageTitle: 'Home Page',
+        year: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my site'
+    })
+});
+
+app.get("/about", (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        year: new Date().getFullYear()
+    });
+});
+app.listen(port, () => {
+    console.log(`Listen to port ${port}`);
+});
