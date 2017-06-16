6419c5cff14ca79db7ee1adec1b0126c98f7f4ac
damith <damithasanka@gmail.com>
2017-06-16 13:01:38 +0530

    add project page

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
-const hsb = require("hbs");
+const hbs = require("hbs");
 const port = process.env.PORT || 3000;
 //app.set('view engine','html');
+hbs.registerPartials(__dirname+"/views/partials");
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear",()=>{
+return new Date().getFullYear();
+});
+
 app.get("/", (req, res) => {
     res.render("home.hbs", {
         pageTitle: 'Home Page',
-        year: new Date().getFullYear(),
-        welcomeMessage: 'Welcome to my site'
+         welcomeMessage: 'Welcome to my site'
     })
 });
-
 app.get("/about", (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page',
-        year: new Date().getFullYear()
+       
+    });
+});
+app.get("/project", (req, res) => {
+    res.render('project.hbs', {
+        pageTitle: 'Project page',
+        message:'Portfolio page here'
     });
 });
 app.listen(port, () => {
